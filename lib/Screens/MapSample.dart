5199e7665a9cc0755deef98672829301e1a936ca
Shadow7258 <pranav.rdoshi@gmail.com>
2021-10-09 21:24:43 +0530

    fix: menu dropdown

--- lib/Screens/MapSample.dart
+++ lib/Screens/MapSample.dart
@@ -39,7 +39,7 @@ class MapSampleState extends State<MapSample> {
   bool full = false;
 
   Future pickImage() async {
-    final pickedFile = await picker.getImage(source: ImageSource.camera);
+    final pickedFile = await picker.pickImage(source: ImageSource.camera);
 
     if (pickedFile != null) {
       setState(() {
--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,14 @@ void main() async {
   runApp(Locanator());
 }
 
-class Locanator extends StatelessWidget {
+class Locanator extends StatefulWidget {
+  @override
+  _LocanatorState createState() => _LocanatorState();
+}
+
+class _LocanatorState extends State<Locanator> {
   final String logo = 'assets/logo.svg';
+  bool dropdownVisibility = true;
 
   @override
   Widget build(BuildContext context) {
@@ -22,23 +28,36 @@ class Locanator extends StatelessWidget {
           toolbarHeight: 70,
           automaticallyImplyLeading: false,
           actions: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(right: 20),
-              child: GestureDetector(
-                onTap: () => {},
-                child: Icon(
-                  Icons.menu,
-                  size: 27,
-                ),
+            PopupMenuButton<String>(
+              color: Color(0xFF44475a),
+              icon: Icon(
+                Icons.menu,
+                size: 27,
               ),
+              itemBuilder: (BuildContext context) {
+                return {'Help', 'Report', 'Settings'}.map((String choice) {
+                  return PopupMenuItem<String>(
+                    value: choice,
+                    child: Row(
+                      children: [
+                        choice == "Help"
+                            ? Icon(Icons.help, color: Colors.white)
+                            : choice == "Settings"
+                                ? Icon(Icons.settings, color: Colors.white)
+                                : Icon(Icons.report, color: Colors.white),
+                        SizedBox(
+                          width: 15,
+                        ),
+                        Text(
+                          choice,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  );
+                }).toList();
+              },
             ),
-            //   TextButton(
-            //       onPressed: () => {},
-            //       child: Text(
-            //         "Menu",
-            //         style: TextStyle(color: Colors.white, fontSize: 18),
-            //       ),
-            //       color: Color(0xFF6272a4))
           ],
           title: Row(
             children: [
@@ -66,3 +85,66 @@ class Locanator extends StatelessWidget {
     );
   }
 }
+
+// class Locanator extends StatelessWidget {
+//   final String logo = 'assets/logo.svg';
+//   final bool dropdownVisibility = false;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Locanator',
+//       home: Scaffold(
+//         appBar: AppBar(
+//           toolbarHeight: 70,
+//           automaticallyImplyLeading: false,
+//           actions: <Widget>[
+//             Padding(
+//               padding: const EdgeInsets.only(right: 20),
+//               child: GestureDetector(
+//                 onTap: () => {},
+//                 child: Icon(
+//                   Icons.menu,
+//                   size: 27,
+//                 ),
+//               ),
+//             ),
+//             Visibility(
+//                 visible: dropdownVisibility,
+//                 child: DropdownButton<String>(
+//                   items: <String>['A', 'B', 'C', 'D'].map((String value) {
+//                     return DropdownMenuItem<String>(
+//                       value: value,
+//                       child: Text(value),
+//                     );
+//                   }).toList(),
+//                   onChanged: (_) {},
+//                 ))
+//           ],
+//           title: Row(
+//             children: [
+//               SvgPicture.asset(
+//                 logo,
+//                 width: 50,
+//               ),
+//               Padding(
+//                 padding: const EdgeInsets.only(left: 12),
+//                 child: Text(
+//                   "Locanator",
+//                   style: TextStyle(
+//                     color: Colors.white,
+//                     fontWeight: FontWeight.w400,
+//                     fontSize: 24,
+//                   ),
+//                 ),
+//               ),
+//             ],
+//           ),
+//           backgroundColor: Color(0xFF282a36),
+//         ),
+//         body: MapSample(),
+//       ),
+//     );
+//   }
+// }
